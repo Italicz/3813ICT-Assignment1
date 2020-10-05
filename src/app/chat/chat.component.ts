63f8774371610d4d099f39a300a30cb58541162d
james.angier <james.angier@thebetmakers.com>
2020-10-05 11:27:09 +1000

    created socket and chat services

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { GroupService } from '../services/group.service';
+import { ChannelService } from '../services/channel.service';
+import { ChatService } from '../services/chat.service';
+import { SocketService } from '../services/socket.service';
+import { Router } from '@angular/router';
 import { User } from '../user';
 
 @Component({
@@ -9,20 +13,47 @@ import { User } from '../user';
 })
 export class ChatComponent implements OnInit {
 
-  constructor(private groupService: GroupService) { }
+  constructor(private groupService: GroupService, private router: Router, private channelService: ChannelService, private chatService: ChatService, private socketService: SocketService) { }
 
   user:User;
   groups:[] = [];
   channels:[] = [];
+  messages = [];
+  newMessage: string;
+  channelName: string;
+  ioConnection: any;
+  msg: any;
+  currentChannel: any;
+  inRoom: boolean;
 
   ngOnInit(): void {
     this.user = JSON.parse(localStorage.getItem('currentUser'));
-    this.getUsersGroups();
+    this.inRoom = false;
+    this.getGroups();
+    this.getChannels();
+    this.socketService.initSocket();
+    this.ioConnection = this.socketService.newNotice().subscribe((msg:string) => {
+      this.messages.push({user: 'Server', message: msg});
+    });
+    this.ioConnection = this.socketService.onMessage().subscribe((msg:string) => {
+      this.messages.push(msg);
+    });
+
+    if (!this.user) {
+      this.router.navigateByUrl('/')
+    }
+
   }
 
-  getUsersGroups() {
-    this.groupService.getUsersGroups(this.user.username).subscribe((data: any) => {
+  getGroups() {
+    this.groupService.getGroups().subscribe((data: any) => {
       this.groups = data;
+    })
+  }
+
+  getChannels() {
+    this.channelService.getChannels().subscribe((data: any) => {
+      this.channels = data;
     })
   }
 
---
+++ src/app/services/socket.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import * as io from 'socket.io-client';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SocketService {
+  private socket;
+
+  initSocket(): void {
+    this.socket = io("http://localhost:3000/chat");
+  }
+
+  public newNotice(): Observable<any> {
+    let observable = new Observable(observer => {
+      this.socket.on('notice', (data:string) => observer.next(data));
+    })
+    return observable;
+  }
+
+  public onMessage(): Observable<any> {
+    let observable = new Observable(observer => {
+      this.socket.on('message', (data:string) => observer.next(data));
+    })
+    return observable;
+  }
+
+  constructor() { }
+}
